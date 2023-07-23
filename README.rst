c7cd9aebd7a6a24f971152d7564e68426e6f4696
me <iwak.sotong@gmail.com>
2023-07-23 19:42:06 +0700

    Execute `fmt` and show the result in Textbox. Add README.

---
+++ README.rst
@@ -0,0 +1,8 @@
+NAME
+  tkfmt.py -- Python/Tkinter based graphical user interface to unix
+  `fmt(1)`_ command.
+
+USAGE
+  ``python3 tkfmt.py``
+
+.. _fmt(1): https://man.freebsd.org/cgi/man.cgi?query=fmt
---
+++ tkfmt.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+import tempfile
+import subprocess
+
+
+class PytkfmtApp:
+    def __init__(self, master=None):
+        # build ui
+        self.MainWindow = tk.Tk() if master is None else tk.Toplevel(master)
+        self.MainWindow.configure(height=800, width=600)
+        self.MainWindow.resizable(True, True)
+        self.MainWindow.title("PyTkFmt")
+
+        main_frame = tk.Frame(self.MainWindow)
+        main_frame.configure(height=800, width=600)
+
+        self.text_input = ScrolledText(main_frame)
+        self.text_input.configure(padx=8, pady=8, )
+        self.text_input.pack(expand=True, fill="both", side="top")
+
+        main_frame.pack(expand=True, fill="both", padx=2, pady=2, side="top")
+
+        self.text_input.bind('<Control-Shift-Return>', self.fmt)
+
+        instruction = 'Paste text here then press \
+Control-Shift-Return to format'
+
+        self.text_input.insert('end', instruction)
+
+        # Main widget
+        self.mainwindow = self.MainWindow
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def get_text(self):
+        return self.text_input.get('1.0', 'end-1c')
+
+    def set_text(self, text):
+        self.text_input.delete('1.0', 'end')
+        self.text_input.insert('end', text)
+
+    def execute_fmt(self, text):
+        tmp = tempfile.NamedTemporaryFile(mode='w')
+        tmp.write(text)
+        tmp.seek(0)
+        result = subprocess.run(['fmt', '-72', tmp.name], capture_output=True)
+        result_text = result.stdout.decode('utf-8').strip()
+        tmp.close()
+        return result_text
+
+    def fmt(self, _evt):
+        text = self.get_text()
+        result_text = self.execute_fmt(text)
+        self.set_text(result_text)
+
+
+if __name__ == "__main__":
+    app = PytkfmtApp()
+    app.run()
